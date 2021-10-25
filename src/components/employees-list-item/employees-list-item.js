11f27c52d0008fee95b7c9d4b5a2376b132f770b
Must <mustafaanvarov@mail.ru>
2021-10-25 15:05:11 +0500

    Added inline styles React

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -14,7 +14,10 @@ const EmployeesListItem = (props) => {
     
         return (
             <li className={classNames}>
-                <span className="list-group-item-label" onClick={onToggleProp} data-toggle="rise">{name}</span>
+                <span className="list-group-item-label"
+                onClick={onToggleProp}
+                data-toggle="rise"
+                style={{fontSize: 40, color: 'red'}}>{name}</span>
                 <input type="text" className="list-group-item-input" defaultValue={salary + '$'} onChange={onSalaryChange}/>
                 <div className='d-flex justify-content-center align-items-center'>
                     <button type="button"
